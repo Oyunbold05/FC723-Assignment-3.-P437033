cc25f471c714d846bfa353d30627ab28e359efd9
Oyunbold05 <148216243+Oyunbold05@users.noreply.github.com>
2024-04-14 21:04:08 +0100

    4th commit. Refactored the application for keyboard input and added invalid-input handling.

--- app.py
+++ app.py
@@ -7,13 +7,25 @@
 
 
 # Application with encapsulation.
-class EuclideanAlgorithm():  # Create class.
-    def euclidean_algorithm(self, num1, num2): # Recursive function as the class method.
-        if num2 == 0:  # Base case. If number2 = 0, number1 is GSD.
+class EuclideanAlgorithm:  # Create class.
+    def gcd_keyboard_input(self):  # The method of the class which takes input via keyboard input.
+        try:
+            number1 = int(input("Enter the 1st positive integer:"))
+            number2 = int(input("Enter the 2nd positive integer:"))
+            if number1 < 0 or number2 < 0:  # Check for valid input (both numbers should be non-negative).
+                raise ValueError("Both numbers should be non-negative integers.")
+            if number1 == 0 and number2 == 0: # Check for valid input (if both numbers are zero, GCD is undefinable.)
+                raise ValueError("Both numbers are 0. GCD undefinable.")
+            return f"Greatest common divisor of given two non-negative integers is {self.gcd(number1, number2)}"
+        except ValueError as error:  # Print error message when invalid input given.
+            print("Invalid input:", error)
+
+    def gcd(self, num1, num2):  # Recursive function as the class method.
+        if num2 == 0:  # Base case. If number2 = 0, number1 is GCD.
             return num1
         else:
-            return self.euclidean_algorithm(num2, num1 % num2)  # Recursive case. Call the function with number2 and the remainder of number1 divided by number2.
+            return self.gcd(num2, num1 % num2)  # Recursive case. Call the function with number2 and the remainder of number1 divided by number2.
 
 
-GSD = EuclideanAlgorithm()
-print(GSD.euclidean_algorithm(192, 270))  # Test. Expected output is 6.
+test = EuclideanAlgorithm()
+print(test.gcd_keyboard_input())
