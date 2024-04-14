c6f6fd0ea57eede224e01816c1cc8fec307e9253
Oyunbold05 <148216243+Oyunbold05@users.noreply.github.com>
2024-04-14 19:33:42 +0100

    1st commit. Designed the pseudocode of the Euclidean Algorithm.

---
+++ app.py
@@ -0,0 +1,6 @@
+# Pseudocode.
+# Function euclidean_algorithm (number1, number2)
+#   if number2 = 0 Then
+#       return number1
+#   else
+#       return euclidean_algorithm (number2, number1 mod number2)
