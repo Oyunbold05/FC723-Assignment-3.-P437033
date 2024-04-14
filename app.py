1f2de0684e1fc66be47f3cf348fe47c6a2ef4dd7
Oyunbold05 <148216243+Oyunbold05@users.noreply.github.com>
2024-04-14 19:54:38 +0100

    3rd commit. Refactored the application with the use of encapsulation.

--- app.py
+++ app.py
@@ -6,12 +6,14 @@
 #       return euclidean_algorithm (number2, number1 mod number2)
 
 
-# Application.
-def euclidean_algorithm(num1, num2):  # Recursive function that implements the Euclidean Algorithm to find GSD of given two positive integers.
-    if num2 == 0:  # Base case. If number2 = 0, number1 is GSD.
-        return num1
-    else:
-        return euclidean_algorithm(num2, num1 % num2)  # Recursive case. Call the function with number2 and the remainder of number1 divided by number2.
+# Application with encapsulation.
+class EuclideanAlgorithm():  # Create class.
+    def euclidean_algorithm(self, num1, num2): # Recursive function as the class method.
+        if num2 == 0:  # Base case. If number2 = 0, number1 is GSD.
+            return num1
+        else:
+            return self.euclidean_algorithm(num2, num1 % num2)  # Recursive case. Call the function with number2 and the remainder of number1 divided by number2.
 
 
-print(euclidean_algorithm(192, 270))  # Test. Expected output is 6.
+GSD = EuclideanAlgorithm()
+print(GSD.euclidean_algorithm(192, 270))  # Test. Expected output is 6.
