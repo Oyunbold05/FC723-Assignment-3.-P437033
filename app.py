2f389b3f9a7d05002212b7d04742b63a3fb9fbca
Oyunbold05 <148216243+Oyunbold05@users.noreply.github.com>
2024-04-14 19:46:33 +0100

    2ndt commit. Converted the pseudocode into a working application.

--- app.py
+++ app.py
@@ -4,3 +4,14 @@
 #       return number1
 #   else
 #       return euclidean_algorithm (number2, number1 mod number2)
+
+
+# Application.
+def euclidean_algorithm(num1, num2):  # Recursive function that implements the Euclidean Algorithm to find GSD of given two positive integers.
+    if num2 == 0:  # Base case. If number2 = 0, number1 is GSD.
+        return num1
+    else:
+        return euclidean_algorithm(num2, num1 % num2)  # Recursive case. Call the function with number2 and the remainder of number1 divided by number2.
+
+
+print(euclidean_algorithm(192, 270))  # Test. Expected output is 6.
